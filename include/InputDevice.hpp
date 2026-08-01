5b0d0837e9c373257a4af76d5005f0c765e0dfd1
Clb184 <84605945+Clb184@users.noreply.github.com>
2026-08-01 12:47:17 -0500

    Add more ways to handle input

--- include/InputDevice.hpp
+++ include/InputDevice.hpp
@@ -5,17 +5,63 @@
 
 class InputDevice {
 public:
+	// Keys for using as IDs in the buffer
+	enum ID_KEYPRESS{
+		IDKP_Z,
+		IDKP_X,
+		IDKP_C,
+		IDKP_SHIFT,
+
+		IDKP_UP,
+		IDKP_DOWN,
+		IDKP_LEFT,
+		IDKP_RIGHT,
+		
+		IDKP_ESCAPE,
+		IDKP_ENTER,
+		IDKP_BACKSPACE,
+		IDKP_SCREENSHOT,
+	};
+
+	enum MOVE_KEY {
+		MK_UP = 1,
+		MK_DOWN = 2,
+		MK_LEFT = 4,
+		MK_RIGHT = 8,
+	};
+
+	enum CONTROL_KEY {
+		CK_Z = 1,
+		CK_X = 2;
+		CK_C = 4;
+		CK_SHIFT = 8;
+	};
+
+public:
 	InputDevice();
 	~InputDevice();
 
 	void Init(GLFWwindow* window_src);
 	void SetKeys();
 	bool GetKeyPress(int code);
+
+	// Specific keys...
+	void Update();
+	bool GetOK(); // Z or Enter
+	bool GetCancel(); // X or Escape
+	uint8_t GetMoveStatus(); // <- v ^ ->
+	uint8_t GetControlStatus(); // Z, X, C, Shift
+	bool GetEscape(); // Escape
+
+
 	void BeginTextInput(char* buffer, size_t size);
+private:
+	bool GetSinglePress(ID_KEYPRESS id, int key_id);
 
 private:
 	GLFWwindow* m_pWindow;
-
+	int m_PastKeyBuffer[IDKP_SCREENSHOT + 1];
+	int m_KeyBuffer[IDKP_SCREENSHOT + 1];
 };
 
 #endif
--- src/InputDevice.cpp
+++ src/InputDevice.cpp
@@ -12,7 +12,8 @@ void TextInput(GLFWwindow* win, unsigned int code) {
 }
 
 InputDevice::InputDevice() {
-
+	memset(m_KeyBuffer, 0x00, sizeof(m_KeyBuffer));
+	memset(m_PastKeyBuffer, 0x00, sizeof(m_PastKeyBuffer));
 }
 
 InputDevice::~InputDevice() {
@@ -31,9 +32,62 @@ bool InputDevice::GetKeyPress(int code) {
 	return GLFW_PRESS == glfwGetKey(m_pWindow, code);
 }
 
+
+void InputDevice::Update() {
+	memcpy(m_PastKeyBuffer, m_KeyBuffer, sizeof(m_PastKeyBuffer));
+
+	m_KeyBuffer[IDKP_Z] == glfwGetKey(m_pWindow, GLFW_KEY_Z);
+	m_KeyBuffer[IDKP_X] == glfwGetKey(m_pWindow, GLFW_KEY_X);
+	m_KeyBuffer[IDKP_C] == glfwGetKey(m_pWindow, GLFW_KEY_C);
+	m_KeyBuffer[IDKP_SHIFT] == glfwGetKey(m_pWindow, GLFW_KEY_SHIFT);
+
+	m_KeyBuffer[IDKP_UP] == glfwGetKey(m_pWindow, GLFW_KEY_UP);
+	m_KeyBuffer[IDKP_DOWN] == glfwGetKey(m_pWindow, GLFW_KEY_DOWN);
+	m_KeyBuffer[IDKP_LEFT] == glfwGetKey(m_pWindow, GLFW_KEY_LEFR);
+	m_KeyBuffer[IDKP_RIGHT] == glfwGetKey(m_pWindow, GLFW_KEY_RIGHT);
+
+	m_KeyBuffer[IDKP_ESCAPE] == glfwGetKey(m_pWindow, GLFW_KEY_ESCAPE);
+	m_KeyBuffer[IDKP_ENTER] == glfwGetKey(m_pWindow, GLFW_KEY_ENTER);
+	m_KeyBuffer[IDKP_BACKSPACE] == glfwGetKey(m_pWindow, GLFW_KEY_BACKSPACE);
+	m_KeyBuffer[IDKP_SCREENSHOT] == glfwGetKey(m_pWindow, GLFW_KEY_F2);
+}
+
+bool InputDevice::GetOK() {
+	return GetSinglePress(IDKP_Z) || GetSinglePress(IDKP_ENTER);
+}
+
+bool InputDevice::GetCancel() {
+	return GetSinglePress(IDKP_X) || GetSinglePress(IDKP_ESCAPE);
+}
+
+uint8_t InputDevice::GetMoveStatus() {
+	return
+		(GLFW_PRESS == m_KeyBuffer[IDKP_UP]) |
+		((GLFW_PRESS == m_KeyBuffer[IDKP_DOWN]) << 1) |
+		((GLFW_PRESS == m_KeyBuffer[IDKP_LEFT]) << 2) |
+		((GLFW_PRESS == m_KeyBuffer[IDKP_RIGHT]) << 3)
+	;
+}
+
+uint32_t InputDevice::GetControlStatus() {
+	return
+		(GLFW_PRESS == m_KeyBuffer[IDKP_Z]) |
+		(GetSinglePress(IDKP_X) << 1) |
+		(GetSinglePress(IDKP_C) << 2) |
+		((GLFW_PRESS == m_KeyBuffer[IDKP_SHIFT]) << 3)
+	;
+}
+
+bool InputDevice::GetEscape() {
+	return GetSinglePress(IDKP_ESCAPE);
+}
+
 void InputDevice::BeginTextInput(char* buffer, size_t size) {
 
 	glfwSetCharCallback(m_pWindow, TextInput);
 }
 
+bool InputDevice::GetSinglePress(ID_KEYPRESS id, int key_id) {
+	return m_PastKeyBuffer[id] == GLFW_PRESS && glfwGetKey(m_pWindow, key_id);
+}
 
