d698d1753fa218bda87f27dc911099025fb9b3c4
Clb184 <84605945+Clb184@users.noreply.github.com>
2026-08-21 01:04:45 -0500

    Test, updating pack bat

--- res/pack.bat
+++ res/pack.bat
@@ -1,1 +1,1 @@
-deps\XPFW\bin\Debug\caf0_pack STG.DAT game.json GRP/*.png DAT/*.DAT DAT/*.vert DAT/*.frag BGM/*.ogg
+deps\XPFW\bin\Debug\caf0_pack STG.DAT GRP/*.png DAT/*.DAT DAT/*.vert DAT/*.frag BGM/*.ogg SFX/*.ogg
